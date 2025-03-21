36ed95d550f773af15833d4b7630b3701af14d91
Onyile Bassey Felix <basseyonyile20044@gmail.com>
2025-03-21 10:42:09 +0100

    Update login.php

--- Login-System-PHP-and-MYSQL-master/Login-System-PHP-and-MYSQL-master/login.php
+++ Login-System-PHP-and-MYSQL-master/Login-System-PHP-and-MYSQL-master/login.php
@@ -26,7 +26,7 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
         header("Location: index.php?error=Password is required");
 	    exit();
 	}else{
-		$sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
+		$sql = " * FROM users WHERE user_name='$uname' AND password='$pass'";
 
 		$result = mysqli_query($conn, $sql);
 		if (mysqli_num_rows($result) === 1) {
